eaa0bb5bbebc2f68f978cc22430064d43b244da5
creamytech <benjaminscott18@gmail.com>
2025-12-11 10:59:31 -0500

    Add Instagram and X social icons to password page

--- app/routes/($locale).password.tsx
+++ app/routes/($locale).password.tsx
@@ -485,8 +485,53 @@ export default function EmailSignupPage() {
           </AnimatePresence>
         </div>
         
+        {/* Social Icons - Below Video */}
+        <div className="relative z-10 flex items-center justify-center gap-6 pb-4">
+          {/* Instagram */}
+          <a 
+            href="https://instagram.com/overgrowth.co" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="text-[#1a472a]/60 hover:text-[#c05a34] transition-colors duration-300"
+            aria-label="Follow us on Instagram"
+          >
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              width="24" 
+              height="24" 
+              viewBox="0 0 24 24"
+            >
+              <path 
+                fill="currentColor" 
+                d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"
+              />
+            </svg>
+          </a>
+          
+          {/* X (Twitter) */}
+          <a 
+            href="https://x.com/Overgrowthco" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="text-[#1a472a]/60 hover:text-[#c05a34] transition-colors duration-300"
+            aria-label="Follow us on X"
+          >
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              width="24" 
+              height="24" 
+              viewBox="0 0 24 24"
+            >
+              <path 
+                fill="currentColor" 
+                d="M18.205 2.25h3.308l-7.227 8.26l8.502 11.24H16.13l-5.214-6.817L4.95 21.75H1.64l7.73-8.835L1.257 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.045 4.126H5.078z"
+              />
+            </svg>
+          </a>
+        </div>
+        
         {/* Bottom spacer for balanced centering */}
-        <div className="h-14 md:h-16" />
+        <div className="h-8 md:h-10" />
       </div>
     </div>
   );
