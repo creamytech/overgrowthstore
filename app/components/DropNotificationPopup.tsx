3742fcb6a81a2e68621e61906cd262fc50ad87a9
creamytech <benjaminscott18@gmail.com>
2026-01-21 17:44:41 -0500

    Increase mobile popup z-index to overlay navbar

--- app/components/DropNotificationPopup.tsx
+++ app/components/DropNotificationPopup.tsx
@@ -188,7 +188,7 @@ export function DropNotificationPopup({ open: externalOpen, onOpenChange }: Drop
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50">
+    <div className="fixed inset-0 z-[9999]">
       {/* Backdrop */}
       <div 
         className="absolute inset-0 bg-black/80"
